4391ef58cbe2a62784f3122ce3e8f530609e5fe1
D23AIML076 RAJESH <d23aiml076@charusat.edu.in>
2025-08-23 08:22:47 +0000

    Finished generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 admin.site.register(Tag)
 
 class PostAdmin(admin.ModelAdmin):
-  prepopulated_fields = {"slug": ("title",)}
-
+    prepopulated_fields = {"slug": ("title",)}
+    
+admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
+
+
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+    title = models.TextField(max_length=100)
+    slug = models.SlugField()
+    summary = models.TextField(max_length=500)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="posts")
+    comments = GenericRelation(Comment)
+
+    def __str__(self):
+        return self.title
